728ab93bbf413ac0a2f84278c277024852d26fe5
MP <47081376+mpiechulek@users.noreply.github.com>
2019-05-19 09:25:30 +0200

    First version of the 24 count down clock

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// jshint esversion:6
+
+//Adding time hh:mm:ss
+const startTime = function() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.querySelector('.clock_digits').innerHTML = `${h}:${m}:${s}`;
+    let t = setTimeout(startTime, 500);
+};
+
+// Adding zero
+const checkTime = function(i) {
+    if (i < 10) {
+		if (1 === i){
+			i = "0 " + i;
+		}	
+        i = "0" + i;
+    }
+    return i;
+};
+
+// loading audio
+const playSound = function() {
+    document.querySelector('.audio').play();
+    let x = setTimeout(playSound, 1);
+    // document.querySelector('.audio').pause();
+};
+
+playSound();
+startTime();
